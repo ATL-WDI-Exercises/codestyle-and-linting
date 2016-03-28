8df244de4be77f28d91cea325fa9a9c3881a1587
Dr. Mike Hopper <drmikeh@gmail.com>
2016-03-28 14:01:55 -0400

    Added solution.js

---
+++ solution.js
@@ -0,0 +1,26 @@
+var myFriends = [
+  'Moe',
+  'Larry',
+  'Curly',
+  'Jane',
+  'Emma',
+  'Elizabeth',
+  'Elinor',
+  'Mary',
+  'Darcy',
+  'Grey',
+  'Lydia',
+  'Harriet'
+];
+
+console.log('My Friends:', myFriends);
+
+if (myFriends.length === 0) {
+  console.log('I\'m feeling lonely.');
+}
+else if (myFriends.length >= 5) {
+  console.log('I have lots of friends');
+}
+else {
+  console.log('I need more friends');
+}
