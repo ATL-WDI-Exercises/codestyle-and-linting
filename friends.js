a8ee477c85be63303a839270c0bf5ea2247a75b9
Dr. Mike Hopper <drmikeh@gmail.com>
2016-07-25 18:44:30 -0400

    Added more code violations to friends.js

--- friends.js
+++ friends.js
@@ -1,4 +1,4 @@
-var my_friends = [
+my_friends = [
   'Moe',
 'Larry', 'Curly',
             'Jane',
@@ -14,11 +14,11 @@ var my_friends = [
 
 console.log('My Friends:', my_friends);
 
-if (my_friends.length===0){ console.log('I\'m feeling lonely.'); }
+if (my_friends.length===0){ console.log('I\'m feeling lonely.') }
 else if(my_friends.length>=5)
   {
-    console.log('I have lots of friends');
+    console.log('I have lots of friends')
 }else{
 
-      console.log('I need more friends');
+      console.log('I need more friends')
            }
