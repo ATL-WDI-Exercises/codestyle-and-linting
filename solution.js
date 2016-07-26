a85de71bae9096cb56f9a865eb50336fa8843bb8
Dr. Mike Hopper <drmikeh@gmail.com>
2016-07-25 21:50:36 -0400

    Updated solution.

--- solution.js
+++ solution.js
@@ -17,10 +17,8 @@ console.log('My Friends:', myFriends);
 
 if (myFriends.length === 0) {
   console.log('I\'m feeling lonely.');
-}
-else if (myFriends.length >= 5) {
+} else if (myFriends.length >= 5) {
   console.log('I have lots of friends');
-}
-else {
+} else {
   console.log('I need more friends');
 }
